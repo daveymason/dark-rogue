a999bce0a78b639394ad6ff70a6dcfbb0eadf702
Dave <daveymason@outlook.com>
2020-11-03 17:55:31 +0800

    Set up React and Canvas
    Set up React app and added a canvas

---
+++ src/DarkRogue.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DarkRogue =() => (
+    <canvas 
+    width="256" 
+    height="256" 
+    style={{border: '3px solid black'}}>
+    </canvas>
+    );
+
+export default DarkRogue
