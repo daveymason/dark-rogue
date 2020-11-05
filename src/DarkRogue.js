a11b6486a9fa2c550248ab2c17ab03810321019c
Dave <daveymason@outlook.com>
2020-11-05 20:56:29 +0800

    Made Square move
    Added key bindings

--- src/DarkRogue.js
+++ src/DarkRogue.js
@@ -1,14 +1,38 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
+import InputManager from './InputManager.js';
 
 const DarkRogue = ({width, height, tilesize}) => {
-    const canvasRef = React.useRef(); //hook
+    const canvasRef = useRef(); //hook
+    const [player, setPlayer] = useState({x:64, y:128}); 
+
+    let inputManager = new InputManager();
+
+    const handleInput = (action, data) => {
+        console.log(`handle input: ${action}:${JSON.stringify(data)}`);
+        let newPlayer = {...player};
+        newPlayer.x += data.x * tilesize;
+        newPlayer.y += data.y * tilesize;
+        setPlayer(newPlayer);
+    };
+
     useEffect(() => {
-        console.log("Draw");
+        console.log('Bind input');
+        inputManager.bindKeys();
+        inputManager.subscribe(handleInput);
+        return () => {
+            inputManager.unbindKeys();
+            inputManager.unsubscribe(handleInput);
+        };
+    });
+
+    useEffect(() => {
+        console.log('Draw');
         const ctx = canvasRef.current.getContext('2d');
         ctx.clearRect(0,0,width*tilesize,height*tilesize);
         ctx.fillStyle = '#000'
-        ctx.fillRect(12,22,16,16);
+        ctx.fillRect(player.x, player.y, 16, 16);
     });
+
     return(
     <canvas 
         ref={canvasRef}
